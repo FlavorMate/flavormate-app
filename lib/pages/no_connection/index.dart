d49f9db26287739b5fbc6d806710ebbc213cbcfc
Chip3211 <github@thenus.de>
2024-09-16 09:26:03 +0200

    - Show the current server on 'no_connection' page

--- lib/pages/no_connection/index.dart
+++ lib/pages/no_connection/index.dart
@@ -6,6 +6,8 @@ import 'package:flavormate/l10n/generated/l10n.dart';
 import 'package:flavormate/riverpod/api/p_api.dart';
 import 'package:flavormate/riverpod/auth_state/p_auth_state.dart';
 import 'package:flavormate/riverpod/go_router/p_go_router.dart';
+import 'package:flavormate/riverpod/shared_preferences/p_server.dart';
+import 'package:flavormate/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -41,27 +43,40 @@ class _NoConnectionPageState extends ConsumerState<NoConnectionPage> {
 
   @override
   Widget build(BuildContext context) {
+    final server = ref.read(pServerProvider);
     return Scaffold(
-      body: Center(
-        child: TColumn(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Stack(
           children: [
-            const Icon(
-              MdiIcons.cloudOffOutline,
-              size: 128,
+            Center(
+              child: TColumn(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    MdiIcons.cloudOffOutline,
+                    size: 128,
+                  ),
+                  TText(
+                    L10n.of(context).p_no_connection_title,
+                    TextStyles.titleLarge,
+                  ),
+                  TText(
+                    L10n.of(context).p_no_connection_subtitle,
+                    TextStyles.titleSmall,
+                    textAlign: TextAlign.center,
+                  ),
+                  FilledButton(
+                    onPressed: () =>
+                        ref.read(pAuthStateProvider.notifier).logout(),
+                    child: Text(L10n.of(context).btn_logout),
+                  ),
+                ],
+              ),
             ),
-            TText(
-              L10n.of(context).p_no_connection_title,
-              TextStyles.titleLarge,
-            ),
-            TText(
-              L10n.of(context).p_no_connection_subtitle,
-              TextStyles.titleSmall,
-              textAlign: TextAlign.center,
-            ),
-            FilledButton(
-              onPressed: () => ref.read(pAuthStateProvider.notifier).logout(),
-              child: Text(L10n.of(context).btn_logout),
+            Positioned(
+              bottom: PADDING,
+              right: PADDING,
+              child: TText('☁ $server', TextStyles.bodyMedium),
             ),
           ],
         ),
