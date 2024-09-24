19d096296ce40260d531442305d50944a83c84e4
Chip3211 <github@thenus.de>
2024-09-24 15:02:28 +0200

    - added new login page

---
+++ lib/components/login/server_form.dart
@@ -0,0 +1,109 @@
+import 'package:flavormate/components/t_button.dart';
+import 'package:flavormate/components/t_text.dart';
+import 'package:flavormate/l10n/generated/l10n.dart';
+import 'package:flavormate/riverpod/shared_preferences/p_server.dart';
+import 'package:flavormate/utils/constants.dart';
+import 'package:flavormate/utils/u_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ServerForm extends ConsumerStatefulWidget {
+  final bool readOnly;
+
+  const ServerForm({required this.readOnly, super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ServerFormState();
+}
+
+class _ServerFormState extends ConsumerState<ServerForm> {
+  final _serverFormKey = GlobalKey<FormState>();
+
+  final _serverController = TextEditingController();
+
+  @override
+  void dispose() {
+    _serverController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: SafeArea(
+        child: TextButton.icon(
+          onPressed: openHelp,
+          icon: const Icon(MdiIcons.helpCircleOutline),
+          label: Text(L10n.of(context).p_login_create_server),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(PADDING),
+              constraints: const BoxConstraints(maxWidth: Breakpoints.sm),
+              child: Form(
+                key: _serverFormKey,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/icons/Transparent.png',
+                      width: 128,
+                      height: 128,
+                    ),
+                    const TText(
+                      'FlavorMate',
+                      TextStyles.headlineLarge,
+                    ),
+                    const SizedBox(height: PADDING * 2),
+                    TextFormField(
+                      enabled: !widget.readOnly,
+                      readOnly: widget.readOnly,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        label: Text(L10n.of(context).p_login_server),
+                      ),
+                      controller: _serverController,
+                      validator: (value) {
+                        if (!UValidator.isHttpUrl(value ?? '')) {
+                          return L10n.of(context).v_isHttpUrl;
+                        }
+
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: PADDING),
+                    SizedBox(
+                      width: 128,
+                      child: TButton(
+                        onPressed: setServer,
+                        label: L10n.of(context).btn_continue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void setServer() {
+    if (!_serverFormKey.currentState!.validate()) return;
+    // TODO: maybe check in the future if a flavormate server is running on the url and only continue if a 'ping' is successful
+    ref.read(pServerProvider.notifier).set(_serverController.text);
+  }
+
+  void openHelp() async {
+    final uri = Uri.parse(FLAVORMATE_GETTING_STARTED);
+    if (await canLaunchUrl(uri)) {
+      launchUrl(uri);
+    }
+  }
+}
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,129 +1,24 @@
-import 'package:flavormate/clients/api_client.dart';
-import 'package:flavormate/components/t_text.dart';
-import 'package:flavormate/extensions/e_build_context.dart';
-import 'package:flavormate/l10n/generated/l10n.dart';
-import 'package:flavormate/models/api/login.dart';
-import 'package:flavormate/riverpod/auth_state/p_auth_state.dart';
+import 'package:flavormate/components/login/login_form.dart';
+import 'package:flavormate/components/login/server_form.dart';
 import 'package:flavormate/riverpod/root_bundle/p_backend_url.dart';
 import 'package:flavormate/riverpod/shared_preferences/p_server.dart';
-import 'package:flavormate/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends ConsumerStatefulWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isStatic = ref.watch(pBackendUrlProvider).requireValue;
+    final serverURL = ref.watch(pServerProvider);
 
-class _LoginPageState extends ConsumerState<LoginPage> {
-  final _serverController = TextEditingController();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  bool _staticServer = false;
-
-  @override
-  void initState() {
-    ref.listenManual(pServerProvider, fireImmediately: true, (_, value) {
-      if (_serverController.text.isNotEmpty) return;
-      _serverController.text = value;
-    });
-
-    _staticServer = ref.read(pBackendUrlProvider).requireValue != null;
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _serverController.dispose();
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.all(PADDING),
-              constraints: const BoxConstraints(maxWidth: Breakpoints.sm),
-              child: AutofillGroup(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/icons/Transparent.png',
-                      width: 128,
-                      height: 128,
-                    ),
-                    const SizedBox(height: PADDING),
-                    TText(L10n.of(context).app_title, TextStyles.headlineLarge),
-                    const SizedBox(height: PADDING * 2),
-                    TextField(
-                      enabled: !_staticServer,
-                      readOnly: _staticServer,
-                      controller: _serverController,
-                      decoration: InputDecoration(
-                        label: Text(L10n.of(context).p_login_server),
-                        border: const OutlineInputBorder(),
-                      ),
-                      autocorrect: false,
-                      onChanged: (value) =>
-                          ref.read(pServerProvider.notifier).set(value),
-                    ),
-                    const SizedBox(height: PADDING),
-                    TextField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        label: Text(L10n.of(context).p_login_username),
-                        border: const OutlineInputBorder(),
-                      ),
-                      autocorrect: false,
-                      autofillHints: const [AutofillHints.username],
-                    ),
-                    const SizedBox(height: PADDING),
-                    TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        label: Text(L10n.of(context).p_login_password),
-                        border: const OutlineInputBorder(),
-                      ),
-                      obscureText: true,
-                      autofillHints: const [AutofillHints.password],
-                    ),
-                    const SizedBox(height: PADDING),
-                    SizedBox(
-                      width: 128,
-                      child: FilledButton(
-                        onPressed: () => login(),
-                        child: Text(L10n.of(context).btn_login),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> login() async {
-    try {
-      await ref.read(pAuthStateProvider.notifier).login(Login(
-            username: _usernameController.text,
-            password: _passwordController.text,
-          ));
-    } on ApiClientException catch (_) {
-      if (!context.mounted) return;
-      context.showTextSnackBar(
-        L10n.of(context).p_login_error,
+    if (serverURL.isEmpty) {
+      return ServerForm(readOnly: isStatic != null);
+    } else {
+      return LoginForm(
+        server: serverURL,
+        isStatic: isStatic != null,
       );
     }
   }
