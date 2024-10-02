3b40ba0f3a33f081ca92bdb6a6c6c755013ed8db
Chip3211 <github@thenus.de>
2024-10-02 13:06:30 +0200

    - Only show stories if feature is enabled

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -2,9 +2,11 @@ import 'package:flavormate/components/dashboard/highlights_viewer.dart';
 import 'package:flavormate/components/dashboard/latest_recipe_viewer.dart';
 import 'package:flavormate/components/dashboard/quick_actions.dart';
 import 'package:flavormate/components/dashboard/stories_viewer.dart';
+import 'package:flavormate/components/riverpod/r_struct.dart';
 import 'package:flavormate/components/t_column.dart';
 import 'package:flavormate/components/t_responsive.dart';
 import 'package:flavormate/components/t_search.dart';
+import 'package:flavormate/riverpod/features/p_feature_story.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,12 +15,19 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const TResponsive(
+    final provider = ref.watch(pFeatureStoryProvider);
+    return TResponsive(
       child: TColumn(
         children: [
           TSearch(),
           QuickActions(),
-          StoriesViewer(),
+          RStruct(
+            provider,
+            (_, enabled) => Visibility(
+              visible: enabled,
+              child: StoriesViewer(),
+            ),
+          ),
           HighlightsViewer(),
           LatestRecipeViewer(),
         ],
