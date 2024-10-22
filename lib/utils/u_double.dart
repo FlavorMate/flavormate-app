8a5fdd470b6eba775737e11e91c0445cb31e9309
Chip3211 <github@thenus.de>
2024-10-23 00:17:25 +0200

    - Add add() method to u_double.dart - Add multiply() method to u_double.dart

--- lib/utils/u_double.dart
+++ lib/utils/u_double.dart
@@ -12,4 +12,14 @@ abstract final class UDouble {
   static bool isPositive(double? value) {
     return value != null && value > 0;
   }
+
+  static double? add(double? a, double? b) {
+    final sum = (a ?? 0) + (b ?? 0);
+    return isPositive(sum) ? sum : null;
+  }
+
+  static double? multiply(double? a, double? b) {
+    final sum = (a ?? 0) * (b ?? 0);
+    return isPositive(sum) ? sum : null;
+  }
 }
