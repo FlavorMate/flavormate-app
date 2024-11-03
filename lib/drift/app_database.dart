a50789014732771be03f4439af6e20cabd66fc54
Chip3211 <github@thenus.de>
2024-11-03 22:03:58 +0100

    - Add StoryDraftTable to Drift database - Update schema version to 2 - Create story_draft_table.dart for new table

--- lib/drift/app_database.dart
+++ lib/drift/app_database.dart
@@ -1,18 +1,34 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:flavormate/drift/tables/draft_table.dart';
+import 'package:flavormate/drift/tables/story_draft_table.dart';
 import 'package:flavormate/models/file/file.dart';
+import 'package:flavormate/models/recipe/recipe.dart';
 import 'package:flavormate/models/recipe_draft/recipe_draft.dart';
 import 'package:flutter/foundation.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [DraftTable])
+@DriftDatabase(tables: [DraftTable, StoryDraftTable])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onCreate: (Migrator m) async {
+        await m.createAll();
+      },
+      onUpgrade: (Migrator m, int from, int to) async {
+        if (from < 2) {
+          await m.createTable(storyDraftTable);
+        }
+      },
+    );
+  }
 
   static QueryExecutor _openConnection() {
     final web = DriftWebOptions(
