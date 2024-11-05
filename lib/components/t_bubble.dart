04ce92fbb49928fdafd54b6bc0fcb66664274e4a
Chip3211 <github@thenus.de>
2024-11-05 16:01:52 +0100

    - Add _Triangle painter for topLeft and topRight decorations

--- lib/components/t_bubble.dart
+++ lib/components/t_bubble.dart
@@ -24,18 +24,51 @@ class TBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color ?? Theme.of(context).colorScheme.primary,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(topLeft ? 0 : BORDER_RADIUS),
-          topRight: Radius.circular(topRight ? 0 : BORDER_RADIUS),
-          bottomLeft: Radius.circular(bottomLeft ? 0 : BORDER_RADIUS),
-          bottomRight: Radius.circular(bottomRight ? 0 : BORDER_RADIUS),
+    final color = this.color ?? Theme.of(context).colorScheme.primary;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (topLeft) CustomPaint(painter: _Triangle(color)),
+        Flexible(
+          child: Container(
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(topLeft ? 0 : BORDER_RADIUS),
+                topRight: Radius.circular(topRight ? 0 : BORDER_RADIUS),
+                bottomLeft: Radius.circular(bottomLeft ? 0 : BORDER_RADIUS),
+                bottomRight: Radius.circular(bottomRight ? 0 : BORDER_RADIUS),
+              ),
+            ),
+            padding: EdgeInsets.all(PADDING),
+            child: TColumn(children: children),
+          ),
         ),
-      ),
-      padding: EdgeInsets.all(PADDING),
-      child: TColumn(children: children),
+        if (topRight) CustomPaint(painter: _Triangle(color)),
+      ],
     );
+  }
+}
+
+class _Triangle extends CustomPainter {
+  final Color color;
+
+  const _Triangle(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..color = color;
+
+    var path = Path();
+    path.lineTo(-5, 0);
+    path.lineTo(0, 10);
+    path.lineTo(5, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
