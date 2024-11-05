3a8eda873a279ba928e328c248e70d74ae94bf44
Chip3211 <github@thenus.de>
2024-11-05 17:14:14 +0100

    - Only show story button if server supports stories

--- lib/pages/more/index.dart
+++ lib/pages/more/index.dart
@@ -1,19 +1,23 @@
+import 'package:flavormate/components/riverpod/r_feature.dart';
 import 'package:flavormate/components/t_card.dart';
 import 'package:flavormate/components/t_column.dart';
 import 'package:flavormate/components/t_icon_button.dart';
 import 'package:flavormate/components/t_responsive.dart';
 import 'package:flavormate/components/t_text.dart';
 import 'package:flavormate/l10n/generated/l10n.dart';
+import 'package:flavormate/riverpod/features/p_feature_story.dart';
 import 'package:flavormate/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class MorePage extends StatelessWidget {
+class MorePage extends ConsumerWidget {
   const MorePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final storyProvider = ref.watch(pFeatureStoryProvider);
     return TResponsive(
       child: TColumn(
         children: [
@@ -60,11 +64,14 @@ class MorePage extends StatelessWidget {
                   icon: MdiIcons.bookPlus,
                   label: L10n.of(context).p_more_recipes,
                 ),
-                TIconButton(
-                  width: 250,
-                  onPressed: () => context.pushNamed('storyDrafts'),
-                  icon: MdiIcons.newspaperVariantOutline,
-                  label: L10n.of(context).p_more_stories,
+                RFeature(
+                  storyProvider,
+                  (_) => TIconButton(
+                    width: 250,
+                    onPressed: () => context.pushNamed('storyDrafts'),
+                    icon: MdiIcons.newspaperVariantOutline,
+                    label: L10n.of(context).p_more_stories,
+                  ),
                 ),
               ],
             ),
