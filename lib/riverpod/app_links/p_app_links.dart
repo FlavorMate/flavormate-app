39f0afc45fd6bead742bed088978cf33ecb5284e
Chip3211 <github@thenus.de>
2024-11-26 12:19:21 +0100

    - added url listener

--- lib/riverpod/app_links/p_app_links.dart
+++ lib/riverpod/app_links/p_app_links.dart
@@ -1,9 +1,7 @@
 import 'package:app_links/app_links.dart';
-import 'package:flavormate/extensions/e_number.dart';
-import 'package:flavormate/extensions/e_uri.dart';
+import 'package:flavormate/models/appLink/app_link.dart';
 import 'package:flavormate/riverpod/go_router/p_go_router.dart';
 import 'package:flavormate/riverpod/shared_preferences/p_server.dart';
-import 'package:flavormate/utils/u_double.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'p_app_links.g.dart';
@@ -24,42 +22,41 @@ class PAppLinks extends _$PAppLinks {
     });
   }
 
+  /// The url contains the following informations:
+  /// - server  -> the server url in base64
+  /// - page    -> the destination page
+  /// - id      -> the destination id (e.g. recipe id)
+  /// - token   -> the token to get temporary access to the server
+  ///
+  /// e.g. `flavormate://open?server=aHR0cDovL2xvY2FsaG9zdDo4MDk1&page=recipe&id=111&token=2222e1ad-76cf-4aa6-ab41-78a2337e6ba5`
   void listener(Uri uri) {
-    /// e.g. flavormate://flavormate.de?action=open&page=recipe&id=1&token=123
-    /// e.g. flavormate://localhost:8095?action=open&page=recipe&id=1&token=123
+    final appLink = AppLink.decode(uri.queryParameters);
 
-    final foreign = uri.socket;
-    final loggedIn = Uri.parse(ref.read(pServerProvider)).socket;
+    final foreign = Uri.parse(appLink.server);
+    final loggedIn = Uri.parse(ref.read(pServerProvider));
 
     if (foreign == loggedIn) {
-      loggedInServer(uri.queryParameters);
+      loggedInServer(uri.host, appLink);
     } else {
-      foreignServer(uri.queryParameters);
+      foreignServer(uri.host, appLink);
     }
   }
 
-  void foreignServer(Map<String, String> params) {
-    if (params['action'] == 'open') {
-      if (params['page'] == 'recipe') {
-        final id = UDouble.tryParsePositive(params['id'] ?? '');
-        final token = params['token'];
-        if (id != null && token != null) {
-          open('public', {
-            'id': id.beautify,
-            'token': token,
-          });
-        }
+  void foreignServer(String action, AppLink appLink) {
+    if (action == AppLinkMode.open.name) {
+      if (appLink.page == 'recipe') {
+        open('public', {
+          'id': appLink.id.toString(),
+          'token': appLink.token,
+        });
       }
     }
   }
 
-  void loggedInServer(Map<String, String> params) {
-    if (params['action'] == 'open') {
-      if (params['page'] == 'recipe') {
-        final id = UDouble.tryParsePositive(params['id'] ?? '');
-        if (id != null) {
-          open('recipe', {'id': id.beautify});
-        }
+  void loggedInServer(String action, AppLink appLink) {
+    if (action == AppLinkMode.open.name) {
+      if (appLink.page == 'recipe') {
+        open('recipe', {'id': appLink.id.toString()});
       }
     }
   }
