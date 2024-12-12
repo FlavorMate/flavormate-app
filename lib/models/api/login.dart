b0e3d124e3d1fbd6a574671cd793a31a26e97d01
Chip3211 <github@thenus.de>
2024-12-12 10:34:54 +0100

    - switched to dart_mappable

--- lib/models/api/login.dart
+++ lib/models/api/login.dart
@@ -1,18 +1,11 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:dart_mappable/dart_mappable.dart';
 
-part 'login.g.dart';
+part 'login.mapper.dart';
 
-@JsonSerializable()
-class Login {
+@MappableClass()
+class Login with LoginMappable {
   final String username;
   final String password;
 
   Login({required this.username, required this.password});
-
-  /// Connect the generated [_$LoginFromJson] function to the `fromJson`
-  /// factory.
-  factory Login.fromJson(Map<String, dynamic> json) => _$LoginFromJson(json);
-
-  /// Connect the generated [_$LoginToJson] function to the `toJson` method.
-  Map<String, dynamic> toJson() => _$LoginToJson(this);
 }
