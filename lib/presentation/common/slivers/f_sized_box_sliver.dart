b2f96470f398755316148080db7e2c361d2d4a85
Chip3211 <github@thenus.de>
2025-12-02 18:14:21 +0100

    - modified widgets

--- lib/presentation/common/slivers/f_sized_box_sliver.dart
+++ lib/presentation/common/slivers/f_sized_box_sliver.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 class FSizedBoxSliver extends StatelessWidget {
   final double? height;
   final double? width;
+  final Widget? child;
 
-  const FSizedBoxSliver({super.key, this.height, this.width});
+  const FSizedBoxSliver({super.key, this.height, this.width, this.child});
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: SizedBox(
         height: height,
         width: width,
+        child: child,
       ),
     );
   }
