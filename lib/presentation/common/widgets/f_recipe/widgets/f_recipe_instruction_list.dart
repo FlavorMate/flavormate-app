9871ab38bf420e9d2394925749bc1154608341c8
Chip3211 <github@thenus.de>
2026-02-23 14:26:36 +0100

    - added ability to check instructions

--- lib/presentation/common/widgets/f_recipe/widgets/f_recipe_instruction_list.dart
+++ lib/presentation/common/widgets/f_recipe/widgets/f_recipe_instruction_list.dart
@@ -1,9 +1,11 @@
 import 'package:collection/collection.dart';
 import 'package:flavormate/core/constants/constants.dart';
+import 'package:flavormate/core/extensions/e_build_context.dart';
 import 'package:flavormate/data/models/local/common_recipe/common_instruction.dart';
 import 'package:flavormate/data/models/local/common_recipe/common_instruction_group.dart';
 import 'package:flavormate/presentation/common/widgets/f_text/f_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 
 class FRecipeInstructionList extends StatelessWidget {
   final CommonInstructionGroup instructionGroup;
@@ -35,7 +37,7 @@ class FRecipeInstructionList extends StatelessWidget {
           Row(
             spacing: PADDING,
             children: [
-              CircleAvatar(child: Text('${instruction.index + 1}.')),
+              _CheckedIndex(instruction: instruction),
               Flexible(child: Text(_parseInstructions(instruction.label))),
             ],
           ),
@@ -63,5 +65,62 @@ class FRecipeInstructionList extends StatelessWidget {
       }
     } while (lIndex != -1);
     return value;
+  }
+}
+
+class _CheckedIndex extends StatefulWidget {
+  const _CheckedIndex({
+    super.key,
+    required this.instruction,
+  });
+
+  final CommonInstruction instruction;
+
+  @override
+  State<StatefulWidget> createState() => _CheckedIndexState();
+}
+
+class _CheckedIndexState extends State<_CheckedIndex> {
+  final double radius = 20;
+  bool _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: .circular(radius),
+      child: Container(
+        width: radius * 2,
+        height: radius * 2,
+        color: context.colorScheme.primaryContainer,
+        child: Stack(
+          fit: .expand,
+          children: [
+            _selected
+                ? const Icon(MdiIcons.check)
+                : Center(
+                    child: FText(
+                      '${widget.instruction.index + 1}.',
+                      style: .titleMedium,
+                      color: .onPrimaryContainer,
+                    ),
+                  ),
+
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: toggle,
+                child: const SizedBox.expand(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void toggle() {
+    setState(() {
+      _selected = !_selected;
+    });
   }
 }
