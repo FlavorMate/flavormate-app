67d7060faf0efb437f5086c450a94ac3e58e190a
Chip3211 <github@thenus.de>
2026-03-15 15:53:52 +0100

    - moved `format`-function to `CommonInstruction`

--- lib/data/models/local/common_recipe/common_instruction.dart
+++ lib/data/models/local/common_recipe/common_instruction.dart
@@ -33,4 +33,28 @@ class CommonInstruction with CommonInstructionMappable {
       index: recipe.index,
     );
   }
+
+  String format(double amountFactor) {
+    var value = label;
+
+    int lIndex = -1;
+    int rIndex = -1;
+    do {
+      lIndex = value.indexOf('[[', lIndex + 1);
+      rIndex = value.indexOf(']]', rIndex + 1);
+
+      if (lIndex != -1) {
+        var foundText = value.substring(lIndex + 2, rIndex);
+        double newValue = double.tryParse(foundText) ?? 1;
+        newValue = newValue * (amountFactor);
+        value = value.replaceAll(
+          '[[$foundText]]',
+          newValue % 1 == 0
+              ? newValue.toStringAsFixed(0)
+              : newValue.toStringAsFixed(2),
+        );
+      }
+    } while (lIndex != -1);
+    return value;
+  }
 }
