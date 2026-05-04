52b3face24cf1bafab9aa79fdd382a0278d4c385
Chip3211 <github@thenus.de>
2026-05-04 16:49:33 +0200

    - New quick actions buttons

--- lib/presentation/common/widgets/f_icon_card.dart
+++ lib/presentation/common/widgets/f_icon_card.dart
@@ -1,3 +1,4 @@
+import 'package:flavormate/core/constants/constants.dart';
 import 'package:flavormate/core/extensions/e_build_context.dart';
 import 'package:flavormate/presentation/common/widgets/f_card.dart';
 import 'package:flavormate/presentation/common/widgets/f_text/f_text.dart';
@@ -6,7 +7,6 @@ import 'package:flutter/material.dart';
 class FIconCard extends StatelessWidget {
   final IconData icon;
   final String label;
-  final double height;
   final double width;
   final double iconSize;
 
@@ -15,7 +15,6 @@ class FIconCard extends StatelessWidget {
   const FIconCard({
     required this.icon,
     required this.label,
-    this.height = 125,
     this.width = 125,
     this.iconSize = 48,
     this.onTap,
@@ -25,24 +24,26 @@ class FIconCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: height,
       width: width,
       child: FCard(
         onTap: onTap,
         child: Column(
+          spacing: PADDING / 2,
           children: [
-            Expanded(
-              child: Icon(
-                color: context.colorScheme.primary,
-                icon,
-                size: iconSize,
-              ),
+            Icon(
+              color: context.colorScheme.primary,
+              icon,
+              size: iconSize,
             ),
-            FText(
-              label,
-              style: FTextStyle.bodyMedium,
-              textAlign: TextAlign.center,
-              color: FTextColor.primary,
+            Expanded(
+              child: Center(
+                child: FText(
+                  label,
+                  style: FTextStyle.bodyMedium,
+                  textAlign: TextAlign.center,
+                  color: FTextColor.primary,
+                ),
+              ),
             ),
           ],
         ),
--- lib/presentation/features/home/widgets/home_quick_actions.dart
+++ lib/presentation/features/home/widgets/home_quick_actions.dart
@@ -5,7 +5,7 @@ import 'package:flavormate/data/models/shared/enums/course.dart';
 import 'package:flavormate/data/repositories/features/accounts/p_rest_accounts_self.dart';
 import 'package:flavormate/data/repositories/features/recipes/p_rest_recipes_random.dart';
 import 'package:flavormate/presentation/common/widgets/f_icon_card.dart';
-import 'package:flavormate/presentation/common/widgets/f_wrap.dart';
+import 'package:flavormate/presentation/common/widgets/f_scrollable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,70 +19,43 @@ class HomeQuickActions extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        double itemSize = _size;
+        return FScrollable(
+          child: Padding(
+            padding: const EdgeInsets.all(PADDING / 2),
+            child: IntrinsicHeight(
+              child: Row(
+                spacing: PADDING / 2,
+                children: [
+                  FIconCard(
+                    width: _size,
+                    icon: MdiIcons.pasta,
+                    label: context.l10n.home_quick_actions__cooking,
+                    onTap: () =>
+                        openSuggestion(context, course: Course.MainDish),
+                  ),
+                  FIconCard(
+                    width: _size,
+                    icon: MdiIcons.cupcake,
+                    label: context.l10n.home_quick_actions__baking,
+                    onTap: () => openSuggestion(context, course: Course.Bakery),
+                  ),
 
-        const max = 4 * _size + 3 * PADDING;
-        const min = 2 * _size + 1 * PADDING;
-
-        if (constraints.maxWidth <= max && constraints.maxWidth >= min) {
-          itemSize = (constraints.maxWidth / 2) - PADDING;
-        }
-
-        return FWrap(
-          children: [
-            SizedBox(
-              width: itemSize,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: FIconCard(
-                  height: _size,
-                  width: _size,
-                  icon: MdiIcons.pasta,
-                  label: context.l10n.home_quick_actions__cooking,
-                  onTap: () => openSuggestion(context, course: Course.MainDish),
-                ),
+                  FIconCard(
+                    width: _size,
+                    icon: MdiIcons.diceMultiple,
+                    label: context.l10n.home_quick_actions__random,
+                    onTap: () => openRecipe(context, ref),
+                  ),
+                  FIconCard(
+                    width: _size,
+                    icon: MdiIcons.bookOpen,
+                    label: context.l10n.home_quick_actions__all_recipes,
+                    onTap: () => context.routes.recipes(),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              width: itemSize,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: FIconCard(
-                  height: _size,
-                  width: _size,
-                  icon: MdiIcons.cupcake,
-                  label: context.l10n.home_quick_actions__baking,
-                  onTap: () => openSuggestion(context, course: Course.Bakery),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: itemSize,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: FIconCard(
-                  height: _size,
-                  width: _size,
-                  icon: MdiIcons.diceMultiple,
-                  label: context.l10n.home_quick_actions__random,
-                  onTap: () => openRecipe(context, ref),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: itemSize,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: FIconCard(
-                  height: _size,
-                  width: _size,
-                  icon: MdiIcons.bookOpen,
-                  label: context.l10n.home_quick_actions__all_recipes,
-                  onTap: () => context.routes.recipes(),
-                ),
-              ),
-            ),
-          ],
+          ),
         );
       },
     );
